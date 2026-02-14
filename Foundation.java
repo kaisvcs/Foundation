2caffef87c25fb45d640645f2313b653af829331
Kai Saephan Villanueva <kaisvillanueva@gmail.com>
2026-02-13 20:48:55 -0800

    Comment out recent practice.

--- Foundation.java
+++ Foundation.java
@@ -10,16 +10,17 @@ public class Foundation {
         String name = "Kai Saephan Villanueva"; // strings
         System.out.println("My name is... " + name);
 
-        int age = 25;   // integers 
-        System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
+        // int age = 25;   // integers 
+        // System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
 
-        float fee = 99.99f;
-        final double PI = 3.1415926;
-        System.out.println("I owe " + fee + " but not " + PI + " in student fees.");
+        // float fee = 99.99f; // float (append with f to save space)
+        // final double PI = 3.1415926; // final + double
+        // System.out.println("I owe " + fee + " but not " + PI + " in student fees.");
         
+        // final int yearDOB = 2000; // final (only for numbers)
+        // System.out.println("I was born in the year... " + yearDOB);
 
-        final int yearDOB = 2000; // final (only for int)
-        System.out.println("I was born in the year... " + yearDOB);
+        
 
     }
 
