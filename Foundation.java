527a04cc81d767c023965e41e9b78864546145bf
Kai Saephan Villanueva <kaisvillanueva@gmail.com>
2026-02-13 20:38:59 -0800

    More foundational practice

--- Foundation.java
+++ Foundation.java
@@ -3,7 +3,19 @@
 public class Foundation {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Syste
+        // Here I will be practicing
+        /* Writing comments */
+
+        // Variables
+        String name = "Kai Saephan Villanueva"; // strings
+        System.out.println("My name is... " + name);
+
+        int age = 25;   // integers 
+        System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
+
+        final int yearDOB = 2000; // final (only for int)
+        System.out.println("I was born in the year... " + yearDOB);
+
     }
 
 }
