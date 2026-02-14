de3637cf8ed18ec86271ed023bc779b0c1f51bb9
Kai Saephan Villanueva <kaisvillanueva@gmail.com>
2026-02-13 20:59:58 -0800

    Comment out practice

--- Foundation.java
+++ Foundation.java
@@ -4,21 +4,28 @@ public class Foundation {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        // Introduction
+        /* Introduction */
         String name = "Kai Saephan Villanueva";
         System.out.println("My name is... " + name);
 
         /* Variables */
-        // int age = 25;   // integers 
-        // System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
-        // float fee = 99.99f; // float (append with f to save space)
-        // final double PI = 3.1415926; // final + double
-        // System.out.println("I owe " + fee + " but not " + PI + " in student fees.");
-        // final int yearDOB = 2000; // final (only for numbers)
-        // System.out.println("I was born in the year... " + yearDOB);
+        /* int age = 25;                        // integers 
+        System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
+        float fee = 99.99f;                     // float (append with f to save space)
+        final double PI = 3.1415926;            // final + double
+        System.out.println("I owe " + fee + " but not " + PI + " in student fees.");
+        final int yearDOB = 2000;               // final (only for numbers)
+        System.out.println("I was born in the year... " + yearDOB); */
 
         /* Primitive Data Types */
+        /* byte humanLife = 100;
+        short tenThousands = 32000;
+        int mostCommon = 20000000;
+        long longBoi = 900000000000000L; // append with L
+        float saveSpace = 3.145f;
+        double forgetEfficency = 3.14159265358979311599796346854;
 
+        System.out.println(humanLife + " " + tenThousands + " " + mostCommon + " " + longBoi + " " + saveSpace + " " + forgetEfficency); */
     }
 
 }
