2e29a9c9a5d3d6fdef04ff28d3fdaa3ce78f8269
Kai Saephan Villanueva <kaisvillanueva@gmail.com>
2026-02-13 20:50:39 -0800

    Fix formatting and practicing common pushing

--- Foundation.java
+++ Foundation.java
@@ -3,24 +3,21 @@
 public class Foundation {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // Here I will be practicing
-        /* Writing comments */
 
-        // Variables
-        String name = "Kai Saephan Villanueva"; // strings
+        // Introduction
+        String name = "Kai Saephan Villanueva";
         System.out.println("My name is... " + name);
 
+        /* Variables */
         // int age = 25;   // integers 
         // System.out.println("I am " + age + " years old. and I will be " + (age + 10) + " years old after 10 years.");
-
         // float fee = 99.99f; // float (append with f to save space)
         // final double PI = 3.1415926; // final + double
         // System.out.println("I owe " + fee + " but not " + PI + " in student fees.");
-        
         // final int yearDOB = 2000; // final (only for numbers)
         // System.out.println("I was born in the year... " + yearDOB);
 
-        
+        /* Primitive Data Types */
 
     }
 
